e621aaf0ea5ae9110fa1b1e79d080119501214ed
Cola Fish <guozhi.china@gmail.com>
2021-01-18 16:07:19 +0800

    Update dephistplt.sh
    add modifcation

--- dephistplt.sh
+++ dephistplt.sh
@@ -1,2 +1,4 @@
+# used to plot focal depth histogram before and after the hypoDD relocatio
+
 cat tmpout.txt | gawk '{if($1=="#"){print $10} }' | pshistogram -R0/40/0/20 -Ba10f5:"Depth(km)":/a5f5:"Frequency"::,%:WSne -JX8c/5c -S -Z1 -W1 -C -L0.5p -V -K > tmpp.ps
 cat hypoDD.reloc | gawk '{print $4}' | pshistogram -R -B  -JX -S -Z1 -W1 -C -L0.5p,red -V -O >> tmpp.ps
